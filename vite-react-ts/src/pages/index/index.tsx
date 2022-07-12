b01ad34ebec41484e5f259d2e044497ad3f1ffd8
zsj <zsj@zsjdeMacBook-Pro-3.local>
2022-07-12 17:11:19 +0800

    开发h5 Headder 组件

--- vite-react-ts/src/pages/index/index.tsx
+++ vite-react-ts/src/pages/index/index.tsx
@@ -1,18 +1,26 @@
-import React, { useEffect, memo } from "react";
-import { Button } from "antd-mobile";
-import k from "./index.module.scss";
-import { login } from "./api";
+import React, { useEffect, memo } from 'react';
+import { Button } from 'antd-mobile';
+import { useHistory } from 'react-router-dom';
+import Header from '../../common/Header';
+import k from './index.module.scss';
+import { login } from './api';
 
+const back = () => {
+  history.back();
+};
 function Index() {
   console.log(11, k);
   useEffect(() => {
     login({
-      username: "zsj",
-      password: "zsj",
+      username: 'zsj',
+      password: 'zsj',
     });
   }, []);
   return (
     <div className={k.kaimoIndex}>
+      <div className="header-wrapper">
+        <Header title="火车票" onBack={back} />
+      </div>
       kaimo 的 index 页面
       <h3>sdsds</h3>
       <div className={k.wrapBox}></div>
