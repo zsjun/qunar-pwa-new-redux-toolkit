f3cbc660f4fd392b2a39704bcc7086a4dd9460a1
zsj <zsoujun@126.com>
2022-07-15 06:55:17 +0800

    开发首页

--- vite-react-ts/src/api/posts.ts
+++ vite-react-ts/src/api/posts.ts
@@ -1,16 +1,16 @@
 //React entry point 会自动根据endpoints生成hooks
-import { baseApi } from "./base";
+import { baseApi } from './base'
 interface IPostVo {
-  id: number;
-  username: string;
-  email: string;
+  id: number
+  username: string
+  email: string
 }
 //使用base URL 和endpoints 定义服务
 const postsApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     // 查询列表
     getPostsList: builder.query<Promise<IPostVo[]>, void>({
-      query: () => "/api/users",
+      query: () => '/api/users',
       // transformResponse: (response: { data: Promise<IPostVo[]> }) => {
       //   console.log(233, response);
       //   return response.data;
@@ -23,14 +23,14 @@ const postsApi = baseApi.injectEndpoints({
     // 创建帖子
     createPosts: builder.mutation({
       query: (data) => ({
-        url: "/api/posts",
-        method: "post",
+        url: '/api/posts',
+        method: 'post',
         body: data,
       }),
     }),
   }),
   overrideExisting: false,
-});
+})
 //导出可在函数式组件使用的hooks,它是基于定义的endpoints自动生成的
 export const {
   useGetPostsListQuery,
@@ -39,5 +39,5 @@ export const {
   // 惰性的查询
   useLazyGetPostsListQuery,
   useLazyGetPostsByIdQuery,
-} = postsApi;
-export default postsApi;
+} = postsApi
+export default postsApi
--- vite-react-ts/src/main.tsx
+++ vite-react-ts/src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import './normalize.css';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { Provider } from 'react-redux'
+import { store } from './app/store'
+import './normalize.css'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -12,4 +12,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <App />
     </Provider>
   </React.StrictMode>
-);
+)
